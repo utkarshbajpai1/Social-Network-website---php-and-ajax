f507fc44a35d8df8eca1a5ac2d775e3ec495b5e5
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-07-14 14:35:08 +0530

    completed the header nav design
    completed the header nav design

--- root/index.php
+++ root/index.php
@@ -2,18 +2,30 @@
 <html>
 	<head>
 		<title>php-Social Network</title>
+		<script src="https://use.fontawesome.com/920a43d2cd.js"></script>
 		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
 		<div id="pageTop">
 			<div id="pageTopWrap">
-
+ 
 				<div id="pageTopLogo">
 					<a href="http:\\www.google.com"> <img src="images/logo.jpg" alt="logo"> </a>
 				</div>
 
 				<div id="pageTopRest">
-					
+					<div id="menu1">
+						<div>
+							<a href="#"> Sign-up / Login </a>
+						</div>
+					</div>
+					<div id="menu2">
+						<div>
+							<a href="#"> <i class="fa fa-home fa-2x" aria-hidden="true"></i> </a>
+							<a href="#"> Menu_Item_1 </a>
+							<a href="#"> Menu_Item_2 </a>
+						</div>
+					</div>
 				</div>
 
 			</div>
