6ba08dca477d3133447cb416d38180149a0a7cbc
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-07-27 17:08:46 +0530

    login

--- root/login.php
+++ root/login.php
@@ -1,3 +1,49 @@
+<?php 
+	if (isset($_POST['e'])) {
+		include_once('php_includes/db_conx.php');
+		$e = mysqli_real_escape_string($db_conx, $_POST['e']);
+
+		include_once('php_includes/crypt.php');
+		$p = cryptPass($_POST['p']);
+
+		$ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
+
+		if ($e == "" || $p == "") {
+			echo 'login_failed';
+			exit()-;
+		}else{
+
+			$sql = 'SELECT id, username, password 
+					FROM users 
+					WHERE email='$e'
+					LIMIT 1';
+			$query = mysqli_query($db_conx, $query);
+			$row   = mysqli_fetch_row($query);
+			$db_id 			= $row[0];
+			$db_username 	= $row[1];
+			$db_pass_str	= $row[2];
+
+			if ($p != $db_pass_str) {
+				echo 'login_failed';
+				exit();
+			}else{
+				$_SESSION['userid'] 	= $db_id;
+				$_SESSION['username'] 	= $db_username;
+				$_SESSION['password'] 	= $db_pass_str;
+
+				setcookie("id", $db_id, strtotime( '+30 days' ), "/", "", "", TRUE);
+				setcookie("user", $db_username, strtotime( '+30 days' ), "/", "", "", TRUE);
+	    		setcookie("pass", $db_pass_str, strtotime( '+30 days' ), "/", "", "", TRUE);
+
+	    		$sql = "UPDATE users SET ip='$ip', lastlogin=now() WHERE username='$db_username' LIMIT 1";
+	            $query = mysqli_query($db_conx, $sql);
+				echo $db_username;
+			    exit();
+ 			}
+
+		}
+ 	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
