47a32e75dede11041045091290527b58f283a145
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-01 23:47:28 +0530

    friend system logic and its implementation
    friend system logic and its implementation

--- root/js/friendSystem.js
+++ root/js/friendSystem.js
@@ -7,7 +7,7 @@ function friendToggle(type, user, elem){
 	var ajax = ajaxObj("POST", "php_parsers/friend_system.php");
 	ajax.onreadystatechange = function(){
 		if(ajaxReturn(ajax) == true){
-			if (ajax.responseText == "friend request sent") {
+			if (ajax.responseText == "friend_request_sent") {
 				_(elem).innerHTML='OK friend request sent';
 			}else if(ajax.responseText == "unfriend_ok"){
 				_(elem).innerHTML = '<button onclick="friendToggle(\'friend\',\'<?php echo $u; ?>\',\'friendBtn\')">Request As Friend</button>';
@@ -19,5 +19,29 @@ function friendToggle(type, user, elem){
 		}
 		ajax.send("type="+type+"&user="+user);
 	}
+
+function blockToggle(type,blockee,elem){
+	var conf = confirm("Press OK to confirm the '"+type+"' action on user <?php echo $u; ?>.");
+	if(conf != true){
+		return false;
+	}
+	var elem = document.getElementById(elem);
+	elem.innerHTML = 'please wait ...';
+	var ajax = ajaxObj("POST", "php_parsers/block_system.php");
+	ajax.onreadystatechange = function() {
+		if(ajaxReturn(ajax) == true) {
+			if(ajax.responseText == "blocked_ok"){
+				elem.innerHTML = '<button onclick="blockToggle(\'unblock\',\'<?php echo $u; ?>\',\'blockBtn\')">Unblock User</button>';
+			} else if(ajax.responseText == "unblocked_ok"){
+				elem.innerHTML = '<button onclick="blockToggle(\'block\',\'<?php echo $u; ?>\',\'blockBtn\')">Block User</button>';
+			} else {
+				alert(ajax.responseText);
+				elem.innerHTML = 'Try again later';
+			}
+		}
+	}
+	ajax.send("type="+type+"&blockee="+blockee);
+}
+
 	
 	
--- root/user.php
+++ root/user.php
@@ -44,6 +44,55 @@ while ($row = mysqli_fetch_array($user_query, MYSQLI_ASSOC)) {
 }
 ?>
 
+<?php
+
+$isFriend 		  = false;
+$ownerBlockViewer = false;
+$viewerBlockOwner = false;
+if($u != $log_username && $user_ok == true){
+	$friend_check = "SELECT id FROM friends WHERE user1='$log_username' AND user2='$u' AND accepted='1' OR user1='$u' AND user2='$log_username' AND accepted='1' LIMIT 1";
+	if(mysqli_num_rows(mysqli_query($db_conx, $friend_check)) > 0){
+        $isFriend = true;
+    }
+	$block_check1 = "SELECT id FROM blockedusers WHERE blocker='$u' AND blockee='$log_username' LIMIT 1";
+	if(mysqli_num_rows(mysqli_query($db_conx, $block_check1)) > 0){
+        $ownerBlockViewer = true;
+    }
+	$block_check2 = "SELECT id FROM blockedusers WHERE blocker='$log_username' AND blockee='$u' LIMIT 1";
+	if(mysqli_num_rows(mysqli_query($db_conx, $block_check2)) > 0){
+        $viewerBlockOwner = true;
+    }
+}
+
+?>
+
+<?php 
+$friend_button = '<button disabled> Request As Friend </button>';
+$block_button  = '<button disabled> Block User </button>';
+
+if($isFriend == true){
+
+	$friend_button = '<button onclick="friendToggle(\'unfriend\',\''.$u.'\',\'friendBtn\')">Unfriend</button>';
+
+} 
+else if($user_ok == true && $u != $log_username && $ownerBlockViewer == false){
+
+	$friend_button = '<button onclick="friendToggle(\'friend\',\''.$u.'\',\'friendBtn\')">Request As Friend</button>';
+
+}
+
+if($viewerBlockOwner == true){
+
+	$block_button = '<button onclick="blockToggle(\'unblock\',\''.$u.'\',\'blockBtn\')">Unblock User</button>';
+
+} 
+else if($user_ok == true && $u != $log_username){
+
+	$block_button = '<button onclick="blockToggle(\'block\',\''.$u.'\',\'blockBtn\')">Block User</button>';
+	
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,7 +102,7 @@ while ($row = mysqli_fetch_array($user_query, MYSQLI_ASSOC)) {
 <link rel="stylesheet" href="css/styles.css">
 <script src="js/main.js"></script>
 <script src="js/ajax.js"></script>
-<script src=" friendSystem.js "></script>
+<script src="js/friendSystem.js "></script>
 </head>
 <body>
 <?php include_once("template_pageTop.php"); ?>
